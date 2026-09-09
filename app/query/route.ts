b6bc8759cb407810f2049e160e14866ddf95450a
Bigyan Neupane <bigyandarshan@gmail.com>
2026-09-09 07:55:19 -0500

    Update route.ts

--- app/query/route.ts
+++ app/query/route.ts
@@ -8,9 +8,15 @@ import postgres from 'postgres';
      FROM invoices
      JOIN customers ON invoices.customer_id = customers.id
      WHERE invoices.amount = 666;
-  `;
+   `;
 
  	return data;
  }
 
-
+export async function GET() {
+   try {
+   	return Response.json(await listInvoices());
+   } catch (error) {
+   	return Response.json({ error }, { status: 500 });
+  }
+}
