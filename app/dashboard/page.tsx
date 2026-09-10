cbc677ba6b611ac5c6749b9618d5636a13d4116e
Bigyan Neupane <bigyandarshan@gmail.com>
2026-09-10 06:43:48 -0500

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,7 +9,7 @@ export default async function Page() {
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Dashboard
+        Tashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
@@ -23,7 +23,7 @@ export default async function Page() {
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         { <RevenueChart revenue={revenue}  /> }
-        { <LatestInvoices latestInvoices={latestInvoices} /> }
+        { // <LatestInvoices latestInvoices={latestInvoices} /> }
       </div>
     </main>
   );
